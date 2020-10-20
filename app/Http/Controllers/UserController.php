da74f3a4a5eea2509ff83ee7c576c89cba42a10a
djcadaniel <djcadaniel@gmail.com>
2020-10-19 23:14:35 -0500

    terminado controlador UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index(){
-        
+        $users = User::latest() ->get();
+        return view('users.index', [
+            'users' => $users
+        ]);
     }
-    public function store(){
-        
+    public function store(Request $request){
+        User::create([
+            'name' => $request -> name,
+            'email' => $request -> email,
+            'password' => $request -> password
+        ]);
+
+        return back();  //retorna a la vista anterior
     }
     public function destroy(){
-        
+        $user -> delete();
+
+        return back();
     }
 }
